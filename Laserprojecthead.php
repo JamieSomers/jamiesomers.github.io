b9c547f41c3a7be6fe5ec28e67849878915a4dc9
Jamie Somers <70515593+JamieSomers@users.noreply.github.com>
2022-08-28 11:02:28 +0100

    Add files via upload

---
+++ Laserprojecthead.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+      <meta http-equiv = "content-language" content = "en">
+      <meta name="description" content="The Laser Clean project consists of three distinct work packages">
+      <meta property="og:site_name" content="Laser Clean">
+      <meta property="og:url" content="https://laserclean.ie">
+      <meta property="og:title" content="Project Work Packages | Laser Clean IE">
+      <meta name="title" content="Project Work Packages | Laser Clean IE">
+      <meta name="keywords" content="Jamie Somers, JamieSomers, jamiesomers, jamie somers, jamie, somers, Jamie, Somers, student, physics, science, labreports, lab reports, real world physics solutions, the physics teacher, quantum physics, real world physics, physics teacher, ucd physics, leaving cert physics, soccer physics, theoretical physics, particle physics, applied physics, institute of physics, ucd school of physics, dcu physics, isaac physics, ucc physics, dcu physics with astronomy, school of physics tcd, physics tcd, physics ucd, applied physics dcu">
+      <meta name="author" content="Laser Clean">
+      <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=10">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+      <meta name="HandheldFriendly" content="true">
+      <meta name="google-site-verification" content="n7-uo8h6o8EC5r7QRxYgXbczQmFycaMY-utYCIlVYwg" />
+      <meta property="og:type" content="article">
+      <meta property="og:image" content="https://laserclean.ie/imgs/HomeThumbnail.jpg" alt="website thumbnail">
+      <meta property="og:image" content="https://laserclean.ie/imgs/HomeThumbnail.png" alt="website thumbnail">
+      <meta property="og:image" content="https://laserclean.ie/imgs/HomeThumbnail.webp" alt="website thumbnail">
+      <meta property="og:image:width" content="1918">
+      <meta property="og:image:height" content="1009">
+      <meta property="twitter:card" content="summary_large_image">
+      <meta name="theme-color" content="#7289DA">
+      <meta property="og:description" content="Jamie Somers is a 3rd year B.Sc in Applied Physics student at DCU. He is interested in Optics & Photonics. Jamie Somers is looking for research opportunities.">
+      <meta name="robots" content="index, follow, max-image-preview:large, max-video-preview:-1">
+      <meta property="og:image" content="https://laserclean.ie/imgs/HomeThumbnail.jpg" alt="website thumbnail">
+      <meta property="og:image" content="https://laserclean.ie/imgs/HomeThumbnail.png" alt="website thumbnail">
+      <meta property="og:image" content="https://laserclean.ie/imgs/HomeThumbnail.webp" alt="website thumbnail">
+      <meta property="og:image:width" content="1918">
+      <meta property="og:image:height" content="1009">
+      <meta property="twitter:url" content="https://laserclean.ie/">
+      <meta property="twitter:title" content="Project Work Packages | Laser Clean IE">
+      <meta property="twitter:description" content="The Laser Clean project consists of three distinct work packages">
+      <meta property="twitter:image" content="https://laserclean.ie/imgs/HomeThumbnail.jpg" alt="website thumbnail">
+      <meta property="twitter:image" content="https://laserclean.ie/imgs/HomeThumbnail.png" alt="website thumbnail">
+      <meta property="twitter:image" content="https://laserclean.ie/imgs/HomeThumbnail.webp" alt="website thumbnail">
+      <meta itemprop="name" content="Laser Clean">
+      <meta itemprop="description" content="The Laser Clean project consists of three distinct work packages">
+      <title><?php print $ptitle ?> </title>
+
+      <link rel="canonical" href="https://laserclean.ie">
+      <link rel="alernate" media="only screen and (max-width: 640px)" href="https://m.laserclean.ie">
+
+      <link rel="stylesheet" type="text/css" href="/css/LaserProjectStyle.css" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+      <link rel="icon" type="image/png"sizes="32x32" href="/favicon-32x32.png">
+      <link rel="icon" type="image/png"sizes="16x16" href="/favicon-16x16.png">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@700&family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css" integrity="sha512-77kidyGDJGWWmJ0MVO0CRp+6nRgZRK67frUVBRvnL1zCcmcw9FkCQxpDHq52SebW+KWTAnnuX0Qk2/MQWogWoQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <script src="nav.js" defer></script>
+      </head>
